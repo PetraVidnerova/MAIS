ca77a53e2f7c2aa803bd8db352e55b157f1e5888
Petra Vidnerova <petra.vidnerova@gmail.com>
2026-01-23 08:49:27 +0100

    one process by default

--- scripts/higgs.sh
+++ scripts/higgs.sh
@@ -1,1 +1,1 @@
-uv run run_multi_experiment.py -R ../config/random_seeds.txt --n_jobs 16 --n_repeat 100 ../config/higgs_sir.ini higgs_sir20
+uv run run_multi_experiment.py -R ../config/random_seeds.txt --n_jobs 1 --n_repeat 100 ../config/higgs_sir.ini higgs_sir20
